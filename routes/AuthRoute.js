d9cdb8cd682519b5227e82c90d4ab30fa8c2a8bc
Jhoel <jhoel@hotmail.es>
2024-08-18 17:21:34 -0500

    sesion con persistencia

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { Login, logOut, Me } = require('../controllers/Auth');
-const { verifyUser} = require('../middleware/AuthUser');
+const { verifyUser, adminOnly } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.get('/me', verifyUser,Me);
+// Ruta para obtener los datos del usuario autenticado
+// Usa el middleware verifyUser para asegurarse de que el usuario está autenticado
+router.get('/me', verifyUser, Me);
+
+// Ruta para iniciar sesión
 router.post('/login', Login);
-router.delete('/logout', logOut);
+
+// Ruta para cerrar sesión
+// Opcionalmente puedes usar verifyUser para asegurarte de que el usuario esté autenticado antes de cerrar sesión
+router.delete('/logout', verifyUser, logOut);
 
 module.exports = router;
