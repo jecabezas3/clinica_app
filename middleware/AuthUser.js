8329ecb8a1f48532c3d48c1bce9f9e189c80f60e
Jhoel <jhoel@hotmail.es>
2024-08-18 13:06:18 -0500

    crossite

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -2,27 +2,20 @@ const User = require("../models/UserModel");
 const sessionStore = require("../index");
 
 exports.verifyUser = async (req, res, next) => {
-    console.log('Session at verifyUser:', req.session); // Muestra toda la sesión
-    console.log('Session ID (sid) at verifyUser:', req.sessionID); // Muestra el SID de la sesión
-    
+    console.log('Session at verifyUser:', req.session);
+    console.log('Session ID (sid) at verifyUser:', req.sessionID);
+
     if (!req.session.userId) {
         return res.status(401).json({ msg: "Primero inicia sesión" });
     }
 
     try {
-        const sessionData = await sessionStore.get(req.sessionID); // Verifica los datos de la sesión
-        console.log('Session Data from Store:', sessionData);
-        
-        if (!sessionData) {
-            return res.status(401).json({ msg: "Sesión no válida o expirada" });
-        }
-
         const user = await User.findOne({
             where: {
                 uuid: req.session.userId
             }
         });
-        
+
         if (!user) {
             return res.status(404).json({ msg: "No existe ese usuario" });
         }
